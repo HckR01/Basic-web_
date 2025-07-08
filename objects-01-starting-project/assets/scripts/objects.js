61fb17c00fe844561f663b7088fce0185085afa8
HckR01 <139985239+HckR01@users.noreply.github.com>
2025-07-08 23:01:40 +0530

    added

---
+++ objects-01-starting-project/assets/scripts/objects.js
@@ -0,0 +1,20 @@
+const person ={
+      nmae:'lax',
+      age:30,
+      hobbies:['sports','cooking'],
+      address:{
+            street:'123 main st',
+            city:'Boston',
+            state:'MA'
+      }
+}
+const person1 ={
+      nmae:'max',
+      age:31,
+      hobbies:['sports','cooking'],
+      address:{
+            street:'123 main st',
+            city:'Boston',
+            state:'MA'
+      }
+}
